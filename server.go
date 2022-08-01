afa6348e75fb6cbd1e1c143cb4dc239ca28922b0
Haruki Ido <rikuharu3705@outlook.jp>
2022-08-01 23:31:14 +0900

    setup initial dev env

--- server.go
+++ server.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"os"
 
 	"golang.org/x/sync/errgroup"
 )
@@ -14,23 +16,29 @@ func hello(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	if err := run(context.Background()); err != nil {
+	if len(os.Args) != 2 {
+		log.Printf("need port number\n")
+	}
+	p := os.Args[1]
+	l, err := net.Listen("tcp", ":"+p)
+	if err != nil {
+		log.Fatalf("failed to listen port %s: %v", p, err)
+	}
+	if err := run(context.Background(), l); err != nil {
 		fmt.Printf("failed to terminate server: %v", err)
 	}
 }
 
-func run(ctx context.Context) error {
+func run(ctx context.Context, l net.Listener) error {
 	http.HandleFunc("/", hello)
-	s := &http.Server{
-		Addr: ":18080",
-	}
+	s := &http.Server{}
 	eg, ctx := errgroup.WithContext(ctx)
 
 	// 別語ルーチンでHTTPサーバを起動する
 	eg.Go(func() error {
 		// http.ErrServerClosedは
 		// http.Server.Shutdown()が正常に終了したことを示すので異常ではない
-		if err := s.ListenAndServe(); err != nil &&
+		if err := s.Serve(l); err != nil &&
 			err != http.ErrServerClosed {
 			log.Printf("failed to close: %+v", err)
 			return err
