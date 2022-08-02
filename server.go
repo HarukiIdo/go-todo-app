00a083a740c187cb7db30f02afe334bd003cb89a
Haruki Ido <rikuharu3705@outlook.jp>
2022-08-03 01:53:32 +0900

    add router

--- server.go
+++ server.go
@@ -11,7 +11,6 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/HarukiIdo/go-todo-app/config"
 	"golang.org/x/sync/errgroup"
 )
 
@@ -20,39 +19,43 @@ type Server struct {
 	l   net.Listener
 }
 
+func NewServer(l net.Listener, mux http.Handler) *Server {
+	return &Server{
+		srv: &http.Server{Handler: mux},
+		l:   l,
+	}
+}
+
 func main() {
-	if err := run(context.Background()); err != nil {
+	srv := Server{}
+	if err := srv.run(context.Background()); err != nil {
 		fmt.Printf("failed to terminate server: %v", err)
 	}
 }
 
-func run(ctx context.Context) error {
+func (s *Server) run(ctx context.Context) error {
 	// グレースフルシャットダウンの実装
 	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
 	defer stop()
 
-	cfg, err := config.New()
-	if err != nil {
-		return nil
-	}
-	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
-	if err != nil {
-		log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
-	}
-	url := fmt.Sprintf("http://%s", l.Addr().String())
-	log.Printf("start with: %v", url)
+	// cfg, err := config.New()
+	// if err != nil {
+	// 	return nil
+	// }
+	// l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
+	// if err != nil {
+	// 	log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
+	// }
+	// url := fmt.Sprintf("http://%s", l.Addr().String())
+	// log.Printf("start with: %v", url)
 
-	// 引数で受けとるnet.Listenerを利用するためAddrフィールドは指定しない
-	s := &http.Server{
-		Handler: http.HandlerFunc(hello),
-	}
 	eg, ctx := errgroup.WithContext(ctx)
 
 	// 別語ルーチンでHTTPサーバを起動する
 	eg.Go(func() error {
 		// http.ErrServerClosedは
 		// http.Server.Shutdown()が正常に終了したことを示すので異常ではない
-		if err := s.Serve(l); err != nil &&
+		if err := s.srv.Serve(s.l); err != nil &&
 			err != http.ErrServerClosed {
 			log.Printf("failed to close: %+v", err)
 			return err
@@ -62,7 +65,7 @@ func run(ctx context.Context) error {
 
 	// チャネルからの通知（終了通知）を待機する
 	<-ctx.Done()
-	if err := s.Shutdown(context.Background()); err != nil {
+	if err := s.srv.Shutdown(context.Background()); err != nil {
 		log.Printf("failed to shutdown: %+v", err)
 	}
 	// Goメソッドで起動した別ゴルーチンの終了を待つ
